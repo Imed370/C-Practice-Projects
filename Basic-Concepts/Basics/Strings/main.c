e2c5cfa1338a6e45cf563b65113e998c538ebf34
Imed370 <141082461+Imed370@users.noreply.github.com>
2024-07-30 18:34:44 +0200

    Strings

--- Basic-Concepts/Basics/Strings/main.c
+++ Basic-Concepts/Basics/Strings/main.c
@@ -26,7 +26,29 @@ int main(){
     char str4[6] = "Hello, World"; // Only "Hello" + '\0' will be stored in str2
     printf("str4: %s\n", str4); // Output will be "Hello"
 
-    //Copying String with strcpy 
+
+    //Taking User Input String Safely with fgets
+    char input[100];
+    printf("Please Enter Your Name:");
+    //Read Input String
+    if(fgets(input, sizeof(input), stdin) != NULL){
+        //Remove newline character if present
+        size_t len = strlen(input);
+        if(len > 0 && input[len -1] == '\n'){
+            input[len -1] = '\0';//Replace with null terminator
+        }
+        printf("You entered:%s\n", input);  
+    }else{
+        printf("Error Reading Input.");
+    }
+
+    //Some Functions of String.h
+
+    //strlen() returns the size of the string without null terminator
+    //if the buffer is 50 and the string has a null terminator at position 13 then it will return 12
+    size_t length_of_string = strlen(str);
+    
+    //strcpy() copies the string to a destination  
     char str5[13];
     strcpy(str5, "Hello World!"); //12 characters + null terminator = 13 spaces required
     printf("str5: %s\n", str5); // Output will be "Hello World!"
@@ -36,18 +58,24 @@ int main(){
     //strcpy(str6, "Hello World"); // Not enough space in the array leads to buffer overflow
     //printf("str6: %s\n", str6); // Undefined Behaviour
 
-    //Copying with strncpy
+    //strncpy() copying more safely 
     //No Overlfow if the array is too short 
     char str7[13];
     strncpy(str7, "Hello, World!", sizeof(str7) - 1);//sizeof() returns size in bytes
     str5[sizeof(str7) - 1] = '\0'; // Ensure null-termination
     printf("%s\n", str7);//Output will be Hello World!
 
-    
-    
+    //strcat() appends the second string to the end of the first string 
+    //can cause buffer overflow if destination does not have enough space
+    char dest[13] = "Hello";
+    char src[] = " World!";
+    strcat(dest,src);
+    printf("Concatenated string:%s", dest);
 
-
-
+    //strncat() safer way of concatenating two strings
+    char dest1[12] = "Hello";
+    char src1[] = " World!";
+    strncat(dest1,src1,7/*Maximum*/); 
     
 
 
