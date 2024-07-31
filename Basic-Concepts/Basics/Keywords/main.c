58c6bba43baf7ac5fb67214229276de3b6cf9aac
Imed370 <imedjannet37@gmail.com>
2024-07-31 17:33:20 +0200

    Added Keywords Topic

---
+++ Basic-Concepts/Basics/Keywords/main.c
@@ -0,0 +1,65 @@
+
+//This topic describes the functionality of some important keywords in c
+
+
+    //Primary Type Keywords
+    /*
+        int 	    Declares an integer variable
+        long 	    Declares a long integer variable
+        short 	    Declares a short integer variable
+        signed 	    Declares a signed variable
+        double 	    Declares a double-precision variable
+        char 	    Declares a character variable
+        float 	    Declares a floating-point variable
+        unsigned 	Declares an unsigned variable
+        void 	    Specifies a void return type
+    */
+
+    
+    //User Defined Type Keywords
+    /*
+        struct 	    Declares a structure type
+        typedef 	Creates a new data type
+        union 	    Declares a union type
+        enum 	    Declares an enumeration type
+    
+    */
+
+    //Storage Type Keywords
+    /*
+        auto 	    Lifetime of the variable is limited to the block its declared in (default)
+        extern 	    Declares a variable or function that is defined in another file
+        static 	    Indicates that a variable retains its value between function calls
+    
+    */
+
+    //Conditional Keywords
+    /*
+        goto 	    Jumps to a labeled statement
+        if 	        Starts an if statement
+        else 	    Executes when the if condition is false
+        case 	    Labels a statement within a switch
+        switch 	    Starts a switch statement
+        default 	Specifies default statement in switch
+    */
+
+
+    //Loop Keywords
+    /*
+        For 	    Starts a for-loop
+        do 	        Starts a do-while loop
+        while 	    starts a while loop
+        continue 	Skips an iteration of a loop
+        break 	    Terminates a loop or switch statement
+    */
+
+    //Other Keywords
+    /*
+        const 	    Specifies a constant value
+        Sizeof 	    Determines the size of a data type
+        Volatile 	tells the compiler that it should always read the value
+                    of the variable from its memory location, rather than
+                    using a cached or optimized value. (used for MMIO, or
+                    variables mapped to HardWare Registers)
+        _Atomic	    Specifies that a type or variable is atomic, meaning it can be accessed concurrently. (C11)
+    */
